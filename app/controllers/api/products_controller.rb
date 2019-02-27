b5d54ebf006f4ef8f9c0228947d69cfd9827fcd3
joeyharbert <joey.harbert@gmail.com>
2019-02-26 20:16:52 -0600

    Adds individual product routes and info

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,9 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render 'all_products.json.jbuilder'
   end
+
+  def show
+    @product = Product.find(params[:id])
+    render 'product.json.jbuilder'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   # end
   namespace :api do
     get "/" => "products#all_products"
+    get 'products/:id' => 'products#show'
   end
 end
