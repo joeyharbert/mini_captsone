6ff097046eefe6e1e962fcc181eaceec0802bd3a
joeyharbert <joey.harbert@gmail.com>
2019-03-14 18:38:59 -0500

    Adds logic to calculate total (with a fixed tax rate of .10) Adds orders index action for logged in user

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,16 +1,36 @@
 class Api::OrdersController < ApplicationController
   def create
     if current_user
+    product = Product.find(params[:product_id])
+    if product
+      price = product.price
+      subtotal = price * params[:quantity].to_i
+      tax = subtotal * 0.10
+      total = subtotal + tax   
+    end
+
     order = Order.new(
       user_id: current_user.id,
       product_id: params[:product_id],
-      quantity: params[:quantity]
+      quantity: params[:quantity],
+      subtotal: subtotal,
+      tax: tax,
+      total: total
       )
       if order.save
         render json: {message: 'Order created successfully'}, status: :created
       else
         render json: {errors: order.errors.full_messages}, status: :bad_request
       end
+    else
+      render json: []
+    end
+  end
+
+  def index
+    if current_user
+      @orders = Order.where(user_id: current_user.id)
+      render 'index.json.jbuilder'
     else
       render json: []
     end
