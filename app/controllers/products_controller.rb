474d60216468c47d285c8f6c9fc5abee2dbe6e8d
joeyharbert <joey.harbert@gmail.com>
2019-03-24 12:07:55 -0500

    Adds image functionality in create, adds update and destroy functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,6 +12,7 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @suppliers = Supplier.all
     render 'new.html.erb'
   end
 
@@ -25,6 +26,47 @@ class ProductsController < ApplicationController
     
     @product.save
 
+    if params[:images].include?(',')
+      images = params[:images].split(', ')
+      images.each do |url|
+        Image.create(url: url, product_id: @product.id)
+      end
+
+    elsif params[:images] != ""
+      Image.create(url: params[:images], product_id: @product.id)
+    end
+
     redirect_to "/products/#{@product.id}"
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+    @suppliers = Supplier.all
+    render 'edit.html.erb'
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(
+      name: params[:name], 
+      price: params[:price], 
+      description: params[:description],
+      supplier_id: params[:supplier_id]
+      )
+    i = 0
+    @product.images.each do |image|
+      image.update(url: params["image_#{i}"])
+      i += 1
+    end
+
+    redirect_to "/products/#{@product.id}"
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+
+    @product.destroy
+
+    redirect_to "/products"
   end
 end
